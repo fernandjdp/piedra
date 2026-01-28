2f1db55c22b1eeea76ee21aab59ee89ca3e62b93
Fernand Jose Diaz Perez <fernand.diaz1708@gmail.com>
2026-01-28 20:10:45 -0300

    fix: mongodb user auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
